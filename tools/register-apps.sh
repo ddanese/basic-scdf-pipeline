ba49a84bc721ef29ce35c3b6f830f341c88cfb47
Dan Danese <ddanese@corelogic.com>
2018-10-31 10:16:54 -0600

    Updated the configs to address issues with mongodb

---
+++ tools/register-apps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+curl -H "Accept: application/json" -X POST "http://localhost:9393/apps/source/source" -i \
+    -d "uri=maven://com.example.pipeline:source:0.0.1-SNAPSHOT" \
+    -d "force=true"
+
+curl -H "Accept: application/json" -X POST "http://localhost:9393/apps/processor/transform" -i \
+    -d "uri=maven://com.example.pipeline:transform:0.0.1-SNAPSHOT" \
+    -d "force=true"
+
+curl -H "Accept: application/json" -X POST "http://localhost:9393/apps/sink/sink" -i \
+    -d "uri=maven://com.example.pipeline:sink:0.0.1-SNAPSHOT" \
+    -d "force=true"
